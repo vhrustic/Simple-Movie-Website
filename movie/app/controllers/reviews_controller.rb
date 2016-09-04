ef19c93835fb235829434517b5e5d7511983849e
Vejsil H <vhrustic1@etf.unsa.ba>
2016-09-04 16:42:41 +0200

    Edit own review

--- movie/app/controllers/reviews_controller.rb
+++ movie/app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+	before_action :authorize
+	before_action :own_review, only: [:edit, :update]
+
 	def create
 		@film = Film.find(params[:film_id]) 
 		@review = Review.create(review_params)
@@ -12,8 +15,29 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def edit
+		@review = get_own_review(params[:id]) #security, better than find then checking is it user's review
+	end
+
+	def update
+		@review = get_own_review(params[:id])
+		if @review.update(review_params)
+			redirect_to @review.film
+		else 
+			render 'edit'
+		end
+	end
+
 	private
 		def review_params
 			params.require(:review).permit(:title, :text, :score)
+		end
+
+		def own_review
+			redirect_to '/films' unless Review.find(params[:id]).user_id == current_user.id
+		end
+
+		def get_own_review(rev_id)
+			Review.where({id: rev_id, user_id: session[:id]}).first
 		end
 end
--- movie/app/models/review.rb
+++ movie/app/models/review.rb
@@ -4,4 +4,8 @@ class Review < ApplicationRecord
   validates :score, presence: true, :numericality => { :greater_than => 0, :less_than_or_equal_to => 10 }
   validates :title, presence: true, length: { in: 3..90 }
   validates :text, presence: true, length: { in: 3..2500 }
+
+  def own_review?(user)
+  	self.user_id == user.id
+  end
 end
